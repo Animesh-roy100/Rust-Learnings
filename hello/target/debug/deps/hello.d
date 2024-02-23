80cc6e98e458d47d1f9cc65e926eb3e5124a3d13
Animesh-roy100 <royanimesh7478@gmail.com>
2024-02-24 02:17:50 +0530

    Running simple hello world using rust

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\legion\OneDrive\Desktop\Projects\Rust\Rust-Learnings\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\legion\OneDrive\Desktop\Projects\Rust\Rust-Learnings\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\legion\OneDrive\Desktop\Projects\Rust\Rust-Learnings\hello\target\debug\hello.exe: C:\Users\legion\OneDrive\Desktop\Projects\Rust\Rust-Learnings\hello\src\main.rs
