06c6da8e3b67a9f38854e5c09daa678c8a38e0ef
Animesh-roy100 <royanimesh7478@gmail.com>
2024-03-31 16:20:11 +0530

    struct_traits added

---
+++ structs_traits/target/debug/deps/structs_traits.d
@@ -0,0 +1,5 @@
+C:\Users\legion\OneDrive\Desktop\Projects\Rust-Learnings\structs_traits\target\debug\deps\structs_traits.exe: src\main.rs
+
+C:\Users\legion\OneDrive\Desktop\Projects\Rust-Learnings\structs_traits\target\debug\deps\structs_traits.d: src\main.rs
+
+src\main.rs:
---
+++ structs_traits/target/debug/structs_traits.d
@@ -0,0 +1,1 @@
+C:\Users\legion\OneDrive\Desktop\Projects\Rust-Learnings\structs_traits\target\debug\structs_traits.exe: C:\Users\legion\OneDrive\Desktop\Projects\Rust-Learnings\structs_traits\src\main.rs
